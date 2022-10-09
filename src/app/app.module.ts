21a5399beea358082d2f34c63ac661548f8d350d
AishwaryaRajput09 <aishwaryanaglot9144@gmail.com>
2022-10-09 21:41:33 +0530

    creating tic-tac-toe game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent,
   ],
   imports: [
     BrowserModule,
